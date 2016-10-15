3e14176c1ff5bc75ca1786db87b25a093b90d132
Alexander Yakovlev <keloero@oreolek.ru>
2016-10-15 13:39:02 +0700

    Update on issue #2: Smartresponder import

--- application/classes/Task/Migrate/Smartresponder.php
+++ application/classes/Task/Migrate/Smartresponder.php
@@ -1,4 +1,7 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
+
+use Ddeboer\Transcoder\Transcoder;
+
  /**
   * This is a task to migrate client database from SmartResponder csv dump
   * It can accept -csv option.
@@ -11,12 +14,14 @@ class Task_Migrate_Smartresponder extends Minion_Task
 {
   protected $_options = array(
     'csv' => NULL,
+    'group' => NULL,
   );
-  
+
   public function build_validation(Validation $validation)
   {
     return parent::build_validation($validation)
-      ->rule('csv', 'not_empty'); // Require this param
+      ->rule('csv', 'not_empty') // Require this param
+      ->rule('group', 'not_empty'); // Require this param
   }
 
   /**
@@ -24,37 +29,58 @@ class Task_Migrate_Smartresponder extends Minion_Task
    *
    * @return null
    */
-  protected function _execute(array $params)
+  protected function _execute()
   {
+    $params = $this->get_options();
     $path = $params['csv'];
-    if (file_exists($path))
+
+    if (!file_exists($path))
     {
-      $db = Database::instance();
-      $db->begin();
-      try
-      {
-        $csv = file($path);
-        foreach ($csv as $line_num => $line)
-        {
-          $arr = explode(',',$line);
-          DB::insert('clients', array('email', 'name'))->values(array(trim($arr[0], '"'), trim($arr[1], '"')))->execute();
+      echo "ERROR: File not found.";
+      return;
+    }
+
+    $db = Database::instance();
+    $db->begin();
+    $transcoder = Transcoder::create();
+
+    $group = ORM::factory('Group')->where('name', '=', $params['group'])->find();
+    if ( ! $group->loaded()) {
+      echo "No group with name " . $params['group'] . " found.\n";
+      return;
+    }
+
+    try
+    {
+      if (($handle = fopen($path, "r")) !== FALSE) {
+        echo "File opened.\n";
+        $query = DB::query(
+          Database::INSERT,
+          'INSERT INTO clients (email, name, group_id)
+          VALUES (:email, :name, :group_id)'
+        )
+          ->bind(':email', $email)
+          ->bind(':name', $name)
+          ->bind(':group_id', $group->id);
+
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+          $email = $data[0];
+          $name = $transcoder->transcode($data[1] . ' ' . $data[2], 'cp1251');
+          $sex = $data[5];
+          $referrer = $data[13];
+          $city = $transcoder->transcode($data[7], 'cp1251');
+          $country = $data[6];
+          echo "Importing client " . $name . ".\n";
+          $query->execute();
         }
-        /*
-        $csv = fopen($path, 'r');
-        while (!feof($csv)) {
-          $string = fgets($csv, 1024);
-          $arr = explode(',',$string);
-          echo $string;
-          DB::insert('clients', array('name', 'email'))->values(array(trim($arr[0], '"'), trim($arr[1], '"')))->execute();
-        }
-        fclose($csv);
-        */
         $db->commit();
+      } else {
+        echo "Could not open the file.";
       }
-      catch(Database_Exception $e)
-      {
-        $db->rollback();
-      }
+    }
+    catch(Database_Exception $e)
+    {
+      $db->rollback();
     }
   }
 }
