6762f6afd08302ae97daea6ce57e922c1cf39a52
Alexander Yakovlev <keloero@oreolek.ru>
2016-11-12 12:27:43 +0700

    Display group id in course editing

--- application/classes/View/Course/Edit.php
+++ application/classes/View/Course/Edit.php
@@ -6,7 +6,7 @@
 class View_Course_Edit extends View_Edit {
   public function custom_controls()
   {
-    return Form::select('group_id', ORM::factory('Group')->find_all()->as_array('id', 'name'), NULL, [
+    return Form::select('group_id', ORM::factory('Group')->find_all()->as_array('id', 'name'), $this->model->group_id, [
       'label' => I18n::translate('Group')
     ]).Form::select('type', Model_Course::type_labels(), $this->model->type, [
       'label' => I18n::translate('Type')
