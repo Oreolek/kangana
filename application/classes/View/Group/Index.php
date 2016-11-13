1a949461f9b149d687ef9944ceb73d8ad4006a83
Alexander Yakovlev <keloero@oreolek.ru>
2016-11-13 20:32:17 +0700

    Link update

--- application/classes/View/Group/Index.php
+++ application/classes/View/Group/Index.php
@@ -30,7 +30,7 @@ class View_Group_Index extends View_Index {
     }
 
     $output = array(
-      'name' => $item->name,
+      'view_link' => HTML::anchor(Route::url('default', array('controller' => Request::current()->controller(), 'action' => 'view','id' => $item->id)), $item->name, array('class' => 'link_view')),
       'edit_link' => HTML::anchor(Route::url('default', array('controller' => Request::current()->controller(), 'action' => 'edit','id' => $item->id)), I18n::translate('Edit'), array('class' => 'link_edit')),
       'delete_link' => HTML::anchor(Route::url('default', array('controller' => Request::current()->controller(), 'action' => 'delete','id' => $item->id)), I18n::translate('Delete'), array('class' => 'link_delete')),
       'client_count' => $item->count_clients(),
